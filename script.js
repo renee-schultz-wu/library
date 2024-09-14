678e62ae54fa307d2a7bf0ba93a0e11b0350b08c
renee <50965960+wurenee@users.noreply.github.com>
2024-09-13 19:55:53 -0700

    Modified how modules are organized

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const myLibrary = [];
 const content = document.querySelector("#content");
 
 const dialog = document.querySelector("dialog");
@@ -11,49 +10,55 @@ const authorInput = document.querySelector(".author");
 const pageInput = document.querySelector(".number");
 
 
+class Book {
+    #name;
+    #author;
+    #page;
+    #dom;
 
-
-
-function Book(name, author, page) {
-  this.name = name;
-  this.author = author;
-  this.page = page;
-
-  this.id = myLibrary.length;
-  
-  this.createSelf = function() {
-    const new_div = document.createElement("div");
-    new_div.innerText = "Book: " + name + " \r\n " + "Author: " + author + "  \r\n " + page + " pages" + "  \r\n "; 
-    new_div.style.backgroundColor = "lightpink";
-    new_div.classList.add("book");
-
-    content.appendChild(new_div);
-    this.dom = new_div;
-  }
-
-  this.addButton = function() {
-    const delete_button = document.createElement("button");
-    delete_button.textContent = "delete";
-    delete_button.classList.add("del");
-    delete_button.addEventListener("click", () => {
-        this.dom.remove();
-        myLibrary[this.id] = undefined;
-    })
-    this.dom.appendChild(delete_button);
-  }
-
-}
-
-function addBookToLibrary(book) {
-    book.createSelf();
-    book.addButton();
-    myLibrary.push(book);
-}
-
-function displayLibrary(){
-    for (let i=0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i])
+    constructor(name, author, page){
+        this.#name = name;
+        this.#author = author;
+        this.#page = page;
     }
+
+    createDom() {
+        const new_div = document.createElement("div");
+        new_div.innerText = "Book: " + this.#name + " \r\n " + "Author: " + this.#author + "  \r\n " + this.#page + " pages" + "  \r\n "; 
+        new_div.classList.add("book");
+    
+        //content.appendChild(new_div);
+        this.#dom = new_div;
+        return new_div;
+    }
+
+    addButton(new_div) {
+        const delete_button = document.createElement("button");
+        delete_button.textContent = "delete";
+        delete_button.classList.add("del");
+        delete_button.addEventListener("click", () => {
+            this.removeSelf();
+            //myLibrary[this.id] = undefined;            
+        })
+        return delete_button;
+    }
+
+    removeSelf() {
+        this.#dom.remove();
+    }
+}
+
+class Library {
+    #booklist = [];
+
+    addBook(book) {
+        let div = book.createDom();
+        let button = book.addButton();
+        div.appendChild(button);
+        content.appendChild(div);
+        this.#booklist.push(book);
+    }
+    
 }
 
 
@@ -68,24 +73,24 @@ closeButton.addEventListener("click", () => {
     dialog.close();
 });
   
-  confirmBtn.addEventListener("click", (event) => {
+confirmBtn.addEventListener("click", (event) => {
     event.preventDefault(); 
     var bname = nameInput.value;
     var bauthor = authorInput.value;
     var bpage = pageInput.value;
     const new_book = new Book(bname, bauthor, bpage);
-    addBookToLibrary(new_book);
+    library.addBook(new_book);
     nameInput.value = "";
     authorInput.value = "";
     pageInput.value = "";
-    dialog.close(); // Have to send the select box value here.
+    dialog.close(); 
 
 });
 
-
+library = new Library();
 const b1 = new Book("Fahrenheit 451", "Ray Bradbury",249)
-addBookToLibrary(b1)
+library.addBook(b1)
 
 const b2 = new Book("Journey to the West", "Wu Cheng'en",2346)
-addBookToLibrary(b2)
+library.addBook(b2)
 
